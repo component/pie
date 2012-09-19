65257ec29e12a07123599b2335714765ba00bff1
TJ Holowaychuk <tj@vision-media.ca>
2012-09-19 16:12:47 -0700

    add stub of pie()

---
+++ index.js
@@ -0,0 +1,18 @@
+
+/**
+ * Expose `pie()`.
+ */
+
+module.exports = pie;
+
+/**
+ * Draw a pie on `ctx` with the given `percent` filled.
+ *
+ * @param {CanvasContext2d} ctx
+ * @param {Number} percent
+ * @api public
+ */
+
+function pie(ctx) {
+  
+}
